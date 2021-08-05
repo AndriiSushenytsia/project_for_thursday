106dc96a13a75b86b19663af511979026da04b0b
AndriiSushenytsia <b11zzard@ukr.net>
2021-08-05 16:24:37 +0300

    update/delete user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,34 @@ class PostsController < ApplicationController
   def new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
       flash[:notice] = "Post has been created!"
       redirect_to root_url
+    else
+      flash[:notice] = "Write valid post please"
+      redirect_to root_url
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post has been updated"
+      redirect_to posts_url
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post has been deleted!"
+    redirect_to posts_url
   end
 
   private
