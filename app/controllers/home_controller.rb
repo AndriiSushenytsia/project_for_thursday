0d8d5bba183b767f79c6ad13124ee4b0c8428257
AndriiSushenytsia <b11zzard@ukr.net>
2021-08-08 14:38:54 +0300

    posts on main, +simple_form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @all_posts = Post.all
+    
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,45 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:edit, :update, :destroy]
-  def show
-  end
 
   def index
-    @post = Post.all
+    @all_posts = Post.all
   end
 
-  def new
-  end
+  def show; end
+
+  def new; end
 
   def edit
-  end
+    @all_posts = Post.all
+   end
 
   def create
     @post = current_user.posts.build(post_params)
+
     if @post.save
       flash[:notice] = "Post has been created!"
       redirect_to root_url
     else
-      flash[:notice] = "Write valid post please"
+      flash[:alert] = @post.errors.full_messages
       redirect_to root_url
     end
   end
 
   def update
+
     if @post.update(post_params)
       flash[:notice] = "Post has been updated"
-      redirect_to posts_url
+      redirect_to root_url
+    else
+      flash[:alert] = @post.errors.full_messages
+      redirect_to edit_post_url
     end
   end
 
   def destroy
     @post.destroy
     flash[:notice] = "Post has been deleted!"
-    redirect_to posts_url
+    redirect_to root_url
   end
 
   private
