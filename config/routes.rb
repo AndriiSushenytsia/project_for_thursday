99fdd2e3bf6c62824abd21b485fe469446503f3e
AndriiSushenytsia <b11zzard@ukr.net>
2021-08-11 14:25:16 +0300

    +namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
+  
+  namespace :current do
+    resources :posts
+  end
+
+  root to: "current/posts#index"
 end
